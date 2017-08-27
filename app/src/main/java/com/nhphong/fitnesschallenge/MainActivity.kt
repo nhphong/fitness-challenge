e3d6496dab43f56489b4d20929bd11aa6d72a75e
Peeqr EA Android <peeqr.ea+android@eastagile.com>
2017-08-27 11:39:17 +0700

    First commit

---
+++ app/src/main/java/com/nhphong/fitnesschallenge/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.nhphong.fitnesschallenge
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.nhphong.fitnesschallenge.databinding.ActivityMainBinding
+
+class MainActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.setVariable(BR.text, "Hello Fitness Challenge")
+        binding.executePendingBindings()
+    }
+}
