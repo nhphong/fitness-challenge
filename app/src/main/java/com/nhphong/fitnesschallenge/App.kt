9820a7de5299f93003167e03212ee20bf3e48838
nhphong <nhphong@apcs.vn>
2017-08-30 01:39:33 +0700

    Set default font

--- app/src/main/java/com/nhphong/fitnesschallenge/App.kt
+++ app/src/main/java/com/nhphong/fitnesschallenge/App.kt
@@ -1,6 +1,9 @@
 package com.nhphong.fitnesschallenge
 
 import android.app.Application
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig
+
+
 
 class App: Application() {
     companion object {
@@ -10,5 +13,10 @@ class App: Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        CalligraphyConfig.initDefault(CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Just tell me what.otf")
+                .setFontAttrId(R.attr.fontPath)
+                .build())
     }
 }
